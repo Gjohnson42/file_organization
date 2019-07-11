2eb4b2dd17a5d11d7e197da0df1ab4eb43988975
Garrett <gjohnson2718@gmail.com>
2019-07-11 15:10:02 +0200

    Okay, the selective copier works. It could probably stand more testing, but it works as intended. Excluding temp files will require a regex to filter them out.

--- selective_copy.py
+++ selective_copy.py
@@ -14,15 +14,29 @@ def gather_input():
 
 def create_destination_folder(new_folder_path, folder_ext_contents):
     edited_folder = new_folder_path + r"\%s" % folder_ext_contents
-    os.makedirs(edited_folder)
+    if not os.path.isdir(edited_folder):
+        os.makedirs(edited_folder)
+    return edited_folder
 
 
 def create_ext_regex(ext_string):
-    ext_regex = re.compile(r"%s" % ext_string)
+    ext_regex = re.compile(ext_string)
+    return ext_regex
 
 
-input_values = gather_input()
-input_values = ['.txt', r'C:\Users\Garrett\Documents\Technical Communication', r"C:\Users\Garrett\Documents"]
-for i in range(len(input_values)):
-    print(input_values[i])
-create_destination_folder(input_values[2], input_values[0])
+# Execution portion
+# input_values = gather_input()
+input_values = ['.docx', r'C:\Users\Garrett\Documents\Old Semester Resources', r"C:\Users\Garrett\Documents"]
+
+new_folder_name = create_destination_folder(input_values[2], input_values[0])
+
+extension_regex = create_ext_regex(input_values[0])
+for folderName, subfolders, filenames in os.walk(input_values[1]):
+
+    for filename in filenames:
+        no_temp_regex = re.compile(r"~")
+        mo_temp = no_temp_regex.search(filename)
+        mo = extension_regex.search(filename)
+        if mo is not None and mo_temp is None:
+            file_dir = os.path.join(folderName, filename)
+            shutil.copy(file_dir, new_folder_name)
